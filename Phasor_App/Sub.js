c736465f9bbb20706f80d90d7e35a82a525538ad
Hung <101842211+tranhungthien@users.noreply.github.com>
2026-01-28 10:25:03 +1100

    Update Sub.js

--- Phasor_App/Sub.js
+++ Phasor_App/Sub.js
@@ -21,29 +21,6 @@ function sub() {
 
     augend = null;
     addend = null;
-    state = "CLR";
+    state = "IDLE";
   }
 }
-// if (augend && addend) {
-//   const px = augend.v.x - augend.u.x;
-//   const py = augend.v.y - augend.u.y;
-
-//   const qx = addend.v.x - addend.u.x;
-//   const qy = addend.v.y - addend.u.y;
-
-//   const Px = px - qx;
-//   const Py = py - qy;
-
-//   const Pmag = sqrt(Px * Px + Py * Py);
-//   // note: everything is flipped upside down on canvas
-//   const a = -(atan2(Py, Px) * 180) / PI;
-//   const Pang = a >= 0 ? a : 360 + a;
-//   let r = new Phasor(Pmag, "darkviolet", "lavender");
-//   r.phasorAngle.value = Pang;
-//   r.rotate();
-//   phasors.push(r);
-
-//   augend = null;
-//   addend = null;
-//   state = "CLR";
-// }
