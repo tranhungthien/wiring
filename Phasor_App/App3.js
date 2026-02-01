19f69d5b39433f63845624464f278bf6240269e4
Hung <101842211+tranhungthien@users.noreply.github.com>
2026-02-01 17:29:13 +1100

    Update App3.js

--- Phasor_App/App3.js
+++ Phasor_App/App3.js
@@ -9,20 +9,12 @@ function setup() {
   history = new History();
 
   initZoom();
-  // phasors.push(new Phasor(200));
-  // phasors[0].phasorAngle.value = 0;
-  // phasors[0].rotate();
-  // phasors.push(new Phasor(200));
-  // phasors[1].phasorAngle.value = 120;
-  // phasors[1].rotate();
-  // phasors.push(new Phasor(200));
-  // phasors[2].phasorAngle.value = 240;
-  // phasors[2].rotate();
-
-  icons.push(new Icon(ox - 0.3 * ox, 100, 64, "-1", "NEG"));
-  icons.push(new Icon(ox - 0.1 * ox, 100, 64, "+", "ADD"));
+  icons.push(new Icon(ox - 0.5 * ox, 100, 64, "-1", "NEG"));
+  icons.push(new Icon(ox - 0.3 * ox, 100, 64, "+", "ADD"));
+  icons.push(new Icon(ox - 0.1 * ox, 100, 64, "×", "MUL"));
   icons.push(new Icon(ox + 0.1 * ox, 100, 64, "-", "SUB"));
-  icons.push(new Icon(ox + 0.3 * ox, 100, 64, "C", "CLR"));
+  icons.push(new Icon(ox + 0.3 * ox, 100, 64, "c", "CNL"));
+  icons.push(new Icon(ox + 0.5 * ox, 100, 64, "C", "CLR"));
   icons.push(new Icon(ox - 0.9 * ox, oy - 0.6 * oy, 64, "v̲", "NEW"));
   icons.push(new Icon(ox - 0.9 * ox, oy - 0.35 * oy, 64, "⚙", "RND"));
 }
